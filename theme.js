290033e82162a0688cde9c4d629f1da2d23ff8e0
Yash Kumar Saini <yash@mail.com>
2023-01-25 22:38:36 +0530

    UI changed with borders removed

--- theme.js
+++ theme.js
@@ -7,11 +7,13 @@ const changeTheme = () => {
     document.documentElement.style.setProperty("--white", "#282828");
     document.documentElement.style.setProperty("--line", "#4d4d4d");
     document.documentElement.style.setProperty("--today", "#1a1a1a");
+    theme.innerHTML = `<span class="material-symbols-outlined">light_mode</span>`;
   } else {
     document.documentElement.style.setProperty("--black", "#282828");
     document.documentElement.style.setProperty("--white", "#fff");
     document.documentElement.style.setProperty("--line", "#d6d6d6");
     document.documentElement.style.setProperty("--today", "#505050");
+    theme.innerHTML = `<span class="material-symbols-outlined">dark_mode</span>`;
   }
   toggle = !toggle;
 };
